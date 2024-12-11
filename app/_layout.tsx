b9d609dc993a4a828da37f9ad4251269f4de4e4a
dustin2 <dustinisrael25@gmail.com>
2024-12-11 10:54:00 -0600

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React from "react";
+import { View, Text } from "react-native";
+// import { Slot } from "expo-router";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+import { Stack } from "expo-router";
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      {/* //al usar  slot de expo router como esta basado en flie system por defesto buscara al archivo index, en cualmmostrara, en este mismo estremos renderizando a n uestros archivos hijos */}
+      {/* <Slot /> */}
+      <Stack.Screen  name="login"/>
+      <Stack.Screen  name="index"/>
+    </Stack>
   );
-}
+};
+
+export default RootLayout;
---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+
+/// get data
+
+//react
+import { Link } from 'expo-router'
+import React from 'react'
+//react native
+import { View, Text } from 'react-native'
+
+//rn paper
+
+
+const codebarScann = () => {
+  return (
+    <View>
+      <Text>codebarScann</Text>
+      <Link href='/login'>go to </Link>
+    </View>
+  )
+}
+
+export default codebarScann
